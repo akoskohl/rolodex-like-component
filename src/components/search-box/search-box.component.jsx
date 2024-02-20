dc04299268951738e190269174e8567a635734ef
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-20 14:33:45 +0100

    prettier

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,19 +1,19 @@
 import { Component } from "react";
-import './search-box.styles.css'
+import "./search-box.styles.css";
 
 class SearchBox extends Component {
-    render() {
-        // why is it now working?
-        const { onSearchChange } = this.props
-        return (
-            <input 
-            className={`search-box ${this.props.className}`}
-            type="search" 
-            placeholder={this.props.placeholder}
-            onChange={this.props.onSearchChange}
-            />
-        )
-    }
+  render() {
+    // why is it now working?
+    const { onSearchChange } = this.props;
+    return (
+      <input
+        className={`search-box ${this.props.className}`}
+        type="search"
+        placeholder={this.props.placeholder}
+        onChange={this.props.onSearchChange}
+      />
+    );
+  }
 }
 
 export default SearchBox;
