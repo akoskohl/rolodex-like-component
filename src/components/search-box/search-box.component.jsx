e121c8b9be89a2783eda1863f1ff6469b21a92cd
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-20 15:11:46 +0100

    Update search-box.component.jsx

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,18 +2,16 @@ import { Component } from "react";
 import "./search-box.styles.css";
 
 class SearchBox extends Component {
-  render() {
-    // why is it now working?
-    const { onSearchChange } = this.props;
-    return (
-      <input
-        className={`search-box ${this.props.className}`}
-        type="search"
-        placeholder={this.props.placeholder}
-        onChange={this.props.onSearchChange}
-      />
-    );
-  }
+    render() {
+        return (
+            <input
+                className={`search-box ${this.props.className}`}
+                type="search"
+                placeholder={this.props.placeholder}
+                onChange={this.props.onSearchChange}
+            />
+        );
+    }
 }
 
 export default SearchBox;
