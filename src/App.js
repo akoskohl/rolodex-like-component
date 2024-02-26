60d86b3106a37f9f91adc77a0f43a48a61a94c25
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-26 17:37:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,16 @@ const App = () => {
   const [searchField, setSearchField] = useState("");
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/akoskohl/scripts/main/random_users_with_email"
+    )
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
         className="monsters-search-box"
-        onSearchChange={onSearchChange} 
         placeholder="search monsters (cmd+f)"
-        />
-        <CardList 
-        monsters={filteredMonsters}
-        />
-      </div>
-    );
-  }
-}
+  );
 };
 
-export default App;
