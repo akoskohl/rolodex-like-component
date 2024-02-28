3a366a2022ef3faca7003e102d46c5d3074dacdb
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-28 10:54:58 +0100

    convert to func component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,16 @@
-import { Component } from "react";
-import './card-list.styles.css'
+import "./card-list.styles.css";
 import Card from "../card/card.component";
 
+const CardList = (props) => {
+  const { monsters } = props;
 
-class CardList extends Component {
-    render() {
-        const { monsters } = this.props
-        
-        return (
-        <div className="card-list">
-            {monsters.map(monster => {
-                return<Card key={monster.id} monster={monster} />
-            })}
-        </div>
-        )
-    }
-}
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <Card key={monsters.id} monster={monster} />;
+      })}
+    </div>
+  );
+};
 
 export default CardList;
